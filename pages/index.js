360bde6a42dfe934dde3e12a18bd70653b2c1e92
Jose Lucio Menezes <106513822+EngJoseLucioMenezes@users.noreply.github.com>
2024-12-16 22:14:10 +0000

    Foi adicinado os arquivos e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Home (){
-    return <h1>Em construção</h1>
+function Home() {
+  return <h1>Em construção</h1>;
 }
 
+function teste1() {
+  console.log("tesste");
+}
 export default Home;
